118670bf7427ca31e9e5c98e063f655bd09a8980
Gazi Md. Noor Hossain <146754153+Fnoor68@users.noreply.github.com>
2024-11-01 15:53:05 +0600

    Created using Colab

---
+++ Calculator.py
@@ -0,0 +1,48 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOIqgm8zN45dzx5wDM62pHg",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Fnoor68/EDGE_Django/blob/main/Calculator.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "id": "Q_i_dmZkA4j1"
+      },
+      "outputs": [],
+      "source": [
+        "def sum(a,b):\n",
+        "    return a+b\n",
+        "def sub(a,b):\n",
+        "    return a-b\n",
+        "def mul(a,b):\n",
+        "    return a*b\n",
+        "def div(a,b):\n",
+        "    return a/b"
+      ]
+    }
+  ]
+}
